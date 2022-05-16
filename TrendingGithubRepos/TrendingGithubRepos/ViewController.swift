562bbdaa496e8ff092142f619adeafe8e9f61da3
Abdullah Tariq <abdullah.tariq@thebyte.app>
2022-05-16 21:29:32 +0500

    project_creation initial_setup

---
+++ TrendingGithubRepos/TrendingGithubRepos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrendingGithubRepos
+//
+//  Created by Abdullah Tariq on 16/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
