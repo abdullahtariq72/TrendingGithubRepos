b38cfbdf1530882487ff7988464ce6170dfa76b4
Abdullah Tariq <abdullah.tariq@thebyte.app>
2022-05-20 16:12:59 +0500

    testcase added

--- TrendingGithubRepos/TrendingGithubReposTests/TrendingGithubReposTests.swift
+++ TrendingGithubRepos/TrendingGithubReposTests/TrendingGithubReposTests.swift
@@ -10,27 +10,17 @@ import XCTest
 
 class TrendingGithubReposTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+   
+    func test_get_repo_list(){
+        let expectation = self.expectation(description: "get_repo_list")
+        RepoListService.shared.getRepoListData(completion: { (repoListResponse: GithubReposListModel?, message: String?) in
+            if let response = repoListResponse{
+                XCTAssertNotNil(response.items)
+            }
+            expectation.fulfill()
+        })
+        waitForExpectations(timeout: 5, handler: nil)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+    
 
 }
--- TrendingGithubRepos/TrendingGithubReposUITests/TrendingGithubReposUITests.swift
+++ TrendingGithubRepos/TrendingGithubReposUITests/TrendingGithubReposUITests.swift
@@ -4,38 +4,54 @@
 //
 //  Created by Abdullah Tariq on 16/05/2022.
 //
-
+import Foundation
 import XCTest
+import TrendingGithubRepos
 
 class TrendingGithubReposUITests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    
+    override func setUp() {
         let app = XCUIApplication()
         app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
+    
+    func testShimmerCell() throws {
+        
+        let table = XCUIApplication().tables
+        XCTAssertNotNil((table.cells.matching(identifier: "listShimmerCell_id")))
+    }
+    
+    func testRepoListCell() throws {
+        
+        let table = XCUIApplication().tables
+        XCTAssertNotNil((table.cells.matching(identifier: "repoListTableViewCell_id")))
+    }
+    
+    func test_Expand_UnExpandCellTap() throws {
+        
+        let app = XCUIApplication()
+        let firstCell = app.tables.cells.element(boundBy: 0)
+        firstCell.tap()
+        let secondCell = app.tables.cells.element(boundBy: 2)
+        secondCell.tap()
+        
+        sleep(3)
+        firstCell.tap()
+        
+        sleep(2)
+        secondCell.tap()
+    }
+    
+    func test_scrollTable_cellTap(){
+        
+        let app = XCUIApplication()
+        let listpagetableviewTable = app.tables
+        
+        let cell = listpagetableviewTable.staticTexts["less.js"]
+        while !cell.exists {
+            app.swipeUp()
         }
+        
+        cell.tap()
     }
 }
